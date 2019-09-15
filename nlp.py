7cf23ce7c2b671ee952ba1a0a3ad15b58dd1338b
Christopher Kyle Horton <christhehorton@gmail.com>
2019-09-15 12:05:21 -0400

    Use ParsedUniversalDependencies in nlp module

--- nlp.py
+++ nlp.py
@@ -3,45 +3,65 @@
 import stanfordnlp
 from stanfordnlp.pipeline.doc import Sentence
 from typing import List, Optional
+from universal_dependencies import ParsedUniversalDependencies
 
 stanfordnlp.download('en')   # This downloads the English models for the neural pipeline
 nlp = stanfordnlp.Pipeline() # This sets up a default neural pipeline in English
 
-class SentenceParsingResults(object):
-    """Represents the results of sentence parsing."""
-
-    def __init__(self, verb: str = None, verb_object: str = None, noun2: str = None, verb2: str = None, preposition: str = None, adjective: str = None):
-        self.verb = verb
-        self.verb_object = verb_object
-        self.noun2 = noun2
-        self.verb2 = verb2
-        self.preposition = preposition,
-        self.adjective = adjective
-
-def Parse(text: str) -> SentenceParsingResults:
+def Parse(text: str) -> ParsedUniversalDependencies:
     """Parses the provided text."""
     sentences = __parse_sentences_from_text(text)
     return __parse_sentence(sentences[0])
 
-def __get_verb(sentence: Sentence) -> Optional[str]:
-    """
-    Returns the verb in the provided sentence as a string, or None if there is no verb.
-    """
-    sentence.print_tokens()
+def __get_word_by_ud_pos(sentence: Sentence, upos: str) -> Optional[str]:
+    """Gets the requested word from the sentence as a string by its Universal
+    Dependencies part-of-speech tag if present, or None if not found."""
+    upos_upper = upos.upper()
     for token in sentence.tokens:
         for word in token.words:
-            if word.pos.startswith('V'):
+            if word.upos.upper() == upos_upper:
                 return word.text
     return None
 
-def __parse_sentence(sentence: Sentence) -> SentenceParsingResults:
+def __parse_sentence(sentence: Sentence) -> ParsedUniversalDependencies:
     """Parses parts of speech from the provided Sentence."""
-    verb = verb_object = noun2 = verb2 = preposition = adjective = None
-    verb = __get_verb(sentence)
-    if verb:
-        # TODO
-    
-    return SentenceParsingResults(verb, verb_object, noun2, verb2, preposition, adjective)
+    sentence.print_tokens()
+    adj = __get_word_by_ud_pos(sentence, "ADJ")
+    adp = __get_word_by_ud_pos(sentence, "ADP")
+    adv = __get_word_by_ud_pos(sentence, "ADV")
+    aux = __get_word_by_ud_pos(sentence, "AUX")
+    verb = __get_word_by_ud_pos(sentence, "VERB")
+    cconj = __get_word_by_ud_pos(sentence, "CCONJ")
+    det = __get_word_by_ud_pos(sentence, "DET")
+    intj = __get_word_by_ud_pos(sentence, "INTJ")
+    noun = __get_word_by_ud_pos(sentence, "NOUN")
+    num = __get_word_by_ud_pos(sentence, "NUM")
+    part = __get_word_by_ud_pos(sentence, "PART")
+    pron = __get_word_by_ud_pos(sentence, "PRON")
+    propn = __get_word_by_ud_pos(sentence, "PROPN")
+    punct = __get_word_by_ud_pos(sentence, "PUNCT")
+    sconj = __get_word_by_ud_pos(sentence, "SCONJ")
+    sym = __get_word_by_ud_pos(sentence, "SYM")
+    verb = __get_word_by_ud_pos(sentence, "VERB")
+    x = __get_word_by_ud_pos(sentence, "X")
+    return ParsedUniversalDependencies(
+        adj = adj,
+        adp = adp,
+        adv = adv,
+        aux = aux,
+        cconj = cconj,
+        det = det,
+        intj = intj,
+        noun = noun,
+        num = num,
+        part = part,
+        pron = pron,
+        propn = propn,
+        punct = punct,
+        sconj = sconj,
+        sym = sym,
+        verb = verb,
+        x = x)
 
 def __parse_sentences_from_text(text: str) -> List[Sentence]:
     """Parses sentences from the provided text."""
@@ -49,4 +69,5 @@ def __parse_sentences_from_text(text: str) -> List[Sentence]:
     return doc.sentences
 
 if __name__ == "__main__":
-    Parse("This is a sample sentence.")
+    parsing_results = Parse("This is a sample sentence.")
+    print(parsing_results.__repr__())
