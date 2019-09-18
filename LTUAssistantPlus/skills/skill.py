b5be9c9b1687d62c9ccbcfe902b743cd94b03511
Christopher Kyle Horton <christhehorton@gmail.com>
2019-09-18 16:54:26 -0400

    Make the Skill class abstract

--- LTUAssistantPlus/skills/skill.py
+++ LTUAssistantPlus/skills/skill.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
+from abc import ABC, abstractmethod
 from nlp.universal_dependencies import ParsedUniversalDependencies
 
-class Skill(object):
-    """Base class for assistant skills."""
+class Skill(ABC):
+    """Abstract base class for assistant skills."""
 
+    @abstractmethod
     def matches_command(self, command_input: ParsedUniversalDependencies) -> bool:
         """Returns a Boolean value indicating whether this skill can be used to handle the given command."""
-        return False
+        pass
     
+    @abstractmethod
     def execute_for_command(self, command_input: ParsedUniversalDependencies, verbose: bool):
         """Executes this skill on the given command input."""
         pass
