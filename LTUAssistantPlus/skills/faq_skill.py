9986b502122469e384b537dcec626426260cce75
Christopher Kyle Horton <christhehorton@gmail.com>
2019-12-12 23:19:39 -0500

    Set up skeleton for FAQSkill helper methods

--- LTUAssistantPlus/skills/faq_skill.py
+++ LTUAssistantPlus/skills/faq_skill.py
@@ -11,23 +11,54 @@ class FAQSkill(Skill):
     def __init__(self):
         """Initializes a new instance of the FAQSkill class."""
         # TODO
-        self._cmd_list = ['what is', 'who is']
+        pass
 
     def matches_command(self, skill_input: SkillInput) -> bool:
         """Returns a Boolean value indicating whether this skill can be used to handle the given command."""
-        # TODO
-        verb = (skill_input.verb or None) and skill_input.verb.lower()
-        return verb in self._cmd_list
+        return self._is_question_for_curriculum(skill_input) or \
+               self._is_question_for_who_has_role(skill_input)
 
     def execute_for_command(self, skill_input: SkillInput, services: AssistantServicesBase):
         """Executes this skill on the given command input."""
-        # TODO
-        pass
+        if self._is_question_for_curriculum(skill_input):
+            self._handle_question_for_curriculum(skill_input)
+        elif self._is_question_for_who_has_role(skill_input):
+            self._handle_question_for_who_has_role(skill_input)
+        else:
+            services.user_interaction_service.speak("Sorry, I cannot answer this kind of question yet.")
 
     def perform_setup(self, services: AssistantServicesBase):
         """Executes any setup work necessary for this skill before it can be used."""
         if not services.settings_service.faq_initialized:
-            # TODO: Perform web crawling and database setup here.
-            raise NotImplementedError("The FAQSkill setup has not been implemented yet.")
+            self._set_up_database()
+            services.settings_service.faq_initialized = True
+            services.settings_service.save_settings()
         else:
             print("FAQ database already initialized.")
+
+    # Various helper functions for this skill are to be defined below.
+
+    def _handle_question_for_curriculum(self, skill_input: SkillInput):
+        """Handles a question regarding the curriculum."""
+        # TODO: Fill this in to have the skill actually process the question and speak the result.
+        pass
+
+    def _handle_question_for_who_has_role(self, skill_input: SkillInput):
+        """Handles a question regarding which staff member has a role."""
+        # TODO: Fill this in to have the skill actually process the question and speak the result.
+        pass
+
+    def _is_question_for_curriculum(self, skill_input: SkillInput) -> bool:
+        """Determines whether the `SkillInput` is for a question regarding the curriculum."""
+        # TODO: Fill this in and return either True or False.
+        pass
+
+    def _is_question_for_who_has_role(self, skill_input: SkillInput) -> bool:
+        """Determines whether the `SkillInput` is for a question regarding which staff member has a role."""
+        # TODO: Fill this in and return either True or False.
+        pass
+
+    def _set_up_database(self):
+        """Sets up the database used for answering questions by crawling the LTU website."""
+        # TODO: Fill this in.
+        raise NotImplementedError("The FAQSkill database setup has not been implemented yet.")
