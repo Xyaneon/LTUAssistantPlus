861f673566f6dab13e8d2880f4f11c9a941bf747
Christopher Kyle Horton <christhehorton@gmail.com>
2019-12-23 10:47:18 -0500

    Improved RoomFinderSkill integration tests using mocks

--- LTUAssistantPlus/test/integration/skills/test_room_finder_skill.py
+++ LTUAssistantPlus/test/integration/skills/test_room_finder_skill.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 import unittest
+from unittest.mock import create_autospec, Mock
 
+from services.assistant_services_base import AssistantServicesBase
+from services.user_interface.user_interaction_service_base import UserInteractionServiceBase
 from skills.skill import SkillInput
 from skills.room_finder_skill import RoomFinderSkill
 from nlp.natural_language_processing import Parse
@@ -27,3 +30,45 @@ class TestRoomFinderSkill(unittest.TestCase):
                 self.skill.matches_command(skill_input),
                 f"RoomFinderSkill did not recognize sentence='{sentence}'\nud: {ud}"
             )
+    
+    def test_skillShouldGiveCorrectOutput(self):
+        sentence = "where is room A101"
+        expected_output_speech = "Your room is in the Architecture Building on floor 1."
+        ud = Parse(sentence)
+        skill_input = SkillInput(ud, False)
+        
+        mock_user_interaction_service = create_autospec(spec=UserInteractionServiceBase)
+        mock_assistant_services = create_autospec(spec=AssistantServicesBase)
+        mock_assistant_services.user_interaction_service.return_value = mock_user_interaction_service
+
+        self.skill.execute_for_command(skill_input, mock_assistant_services)
+
+        mock_assistant_services.user_interaction_service.speak.assert_called_with(expected_output_speech, False)
+    
+    def test_skillShouldRejectInvalidBuildingLetter(self):
+        sentence = "where is room Z101"
+        expected_output_speech = "Sorry, I don't think you provided me with a valid room number."
+        ud = Parse(sentence)
+        skill_input = SkillInput(ud, False)
+        
+        mock_user_interaction_service = create_autospec(spec=UserInteractionServiceBase)
+        mock_assistant_services = create_autospec(spec=AssistantServicesBase)
+        mock_assistant_services.user_interaction_service.return_value = mock_user_interaction_service
+
+        self.skill.execute_for_command(skill_input, mock_assistant_services)
+
+        mock_assistant_services.user_interaction_service.speak.assert_called_with(expected_output_speech, False)
+    
+    def test_skillShouldRejectMissingRoomNumber(self):
+        sentence = "where is room"
+        expected_output_speech = "Sorry, I don't think you provided me with a valid room number."
+        ud = Parse(sentence)
+        skill_input = SkillInput(ud, False)
+        
+        mock_user_interaction_service = create_autospec(spec=UserInteractionServiceBase)
+        mock_assistant_services = create_autospec(spec=AssistantServicesBase)
+        mock_assistant_services.user_interaction_service.return_value = mock_user_interaction_service
+
+        self.skill.execute_for_command(skill_input, mock_assistant_services)
+
+        mock_assistant_services.user_interaction_service.speak.assert_called_with(expected_output_speech, False)
