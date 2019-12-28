0f819d32298c41f1a44493c229d5a7efa0b26ec0
Christopher Kyle Horton <christhehorton@gmail.com>
2019-12-27 23:42:02 -0500

    Make verbosity controllable via command line switch

--- LTUAssistantPlus/LTUAssistant.py
+++ LTUAssistantPlus/LTUAssistant.py
@@ -9,7 +9,7 @@ from services.assistant_services_base import AssistantServicesBase
 from skills import init_skills
 
 
-def process_command(services: AssistantServicesBase, optional_message: str = None):
+def process_command(services: AssistantServicesBase, optional_message: str = None, verbose: bool = False):
     """Processes a command, either supplied as a parameter or obtained from
     user interaction."""
     if optional_message:
@@ -22,7 +22,7 @@ def process_command(services: AssistantServicesBase, optional_message: str = Non
             services.user_interaction_service.tell_user_could_not_be_heard()
             return
     ud = Parse(sentence)
-    if not skill_selection.identify_and_run_command(sentence, ud, services):
+    if not skill_selection.identify_and_run_command(sentence, ud, services, verbose):
         services.user_interaction_service.tell_user_command_was_not_understood()
 
 
@@ -30,6 +30,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--text-only-mode',
                         help='make all user interaction happen in the terminal',
+                        action='store_true')
+    parser.add_argument('-v', '--verbose',
+                        help='show verbose output in the terminal',
                         action='store_true')
     parser.add_argument('-c', '--command-string',
                         help='user\'s initial command text in string form',
@@ -40,6 +43,6 @@ if __name__ == "__main__":
     init_skills(services)
 
     if args.command_string:
-        process_command(services, args.command_string)
+        process_command(services, args.command_string, args.verbose)
     else:
         process_command(services)
